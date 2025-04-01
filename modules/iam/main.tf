fb4c05682e9d28ad9e027d154df502d350575abd
Awez123 <awezkhan6889@gmail.com>
2025-04-02 02:18:53 +0530

    chagnes

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -21,6 +21,26 @@ resource "aws_iam_role_policy_attachment" "cloudwatch" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+resource "aws_iam_role_policy" "cloudwatch_custom" {
+  name   = "CloudWatchCustomPolicy"
+  role   = aws_iam_role.ec2_ssm_role.name
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "cloudwatch:PutMetricData",
+        "cloudwatch:GetMetricData",
+        "cloudwatch:ListMetrics",
+        "logs:PutLogEvents",
+        "logs:CreateLogStream",
+        "logs:DescribeLogStreams"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = var.instance_profile_name
   role = aws_iam_role.ec2_ssm_role.name
